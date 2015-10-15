6208179e47a9b2a0f2ab4a8f0dd493122e8cf7fd
Kakz <manoj.chary@gmail.com>
2015-10-16 02:23:29 +0800

    Update urls.py

--- companydetails/urls.py
+++ companydetails/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from companydetails.views import CompanyList, CompanyListDetail, OwnerList, OwnerListDetail
+from companydetails.views import CompanyList, CompanyListDetail
 # from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
