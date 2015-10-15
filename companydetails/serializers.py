2279c93ec611db788930b0bbcddc6e11f84e3b27
Kakz <manoj.chary@gmail.com>
2015-10-16 02:24:37 +0800

    Update serializers.py

--- companydetails/serializers.py
+++ companydetails/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CompanyDetails, OwnerDetails
+from .models import CompanyDetails
 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
 	id = serializers.IntegerField(read_only=True)
@@ -10,11 +10,10 @@ class CompanySerializer(serializers.HyperlinkedModelSerializer):
 	country = serializers.CharField(max_length=100)
 	email = serializers.EmailField(max_length=100)
 	phone = serializers.CharField(max_length=100)
-	url = serializers.HyperlinkedIdentityField(view_name='owner-list')
 	
 	class Meta:
 	    model = CompanyDetails
-	    fields = ('url', 'id', 'company_id', 'name', 'address', 'city', 'country', 'email', 'phone')
+	    fields = ('id', 'company_id', 'name', 'address', 'city', 'country', 'email', 'phone')
 
 	def create(self, validated_data):
 		return CompanyDetails.objects.create(
@@ -38,27 +37,3 @@ class CompanySerializer(serializers.HyperlinkedModelSerializer):
 		instance.country = validated_data.get('country', instance.country)
 		instance.email = validated_data.get('email', instance.email)
 		instance.phone = validated_data.get('phone', instance.phone)
-
-class OwnerSerializer(serializers.Serializer):
-	id = serializers.IntegerField(read_only=True)
-	company_id = serializers.Field(source='companydetails.id')
-	name = serializers.CharField(max_length=100)
-
-	# class Meta:
-	#     model = CompanyDetails
-	#     fields = ('company_id', 'name')
-
-	def create(self, validated_data):
-		return CompanyDetails.objects.create(
-			company_id = validated_data['company_id'],
-			name = validated_data['name'],
-			)
-
-	def update(self, instance, validated_data):
-		"""
-		Update and return an existing `Company` instance, given the validated data.
-		"""
-		instance.company_id = validated_data.get('company_id', instance.company_id)
-		instance.name = validated_data.get('name', instance.name)
-		instance.save()
-		return instance
